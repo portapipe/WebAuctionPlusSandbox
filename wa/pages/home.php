c8dfe74e5ef2ee4613aac9e08cbf62c190422306
lorenzo42p <lorenzo42p@c41ed8a6-1c15-8a3d-a996-777bd911872f>
2013-01-29 17:08:34 +0000

    added custom.css.optional file added css button class, and formatting for input haven't committed wa/html/default/main.html.php in a while, oops   using new bootstrap_NavBar class   renamed _footer() function to _foot() to avoid conflicts with blocks in memory   many other bug fixes and clean up   commented out xhtml logo - looks better without it added some test data to home.php page - will remove this page
    git-svn-id: http://webauctionplus.googlecode.com/svn/sandbox@303 c41ed8a6-1c15-8a3d-a996-777bd911872f

--- wa/pages/home.php
+++ wa/pages/home.php
@@ -1,14 +1,53 @@
 <?php namespace wa;
-if(!defined('PORTAL_INDEX_FILE')){if(headers_sent()){echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';}else{header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
+if(!defined('\PORTAL_INDEX_FILE') || \PORTAL_INDEX_FILE!==TRUE){if(headers_sent()){echo '<header><meta http-equiv="refresh" content="0;url=../"></header>';}else{header('HTTP/1.0 301 Moved Permanently'); header('Location: ../');} die("<font size=+2>Access Denied!!</font>");}
 class page_home extends \psm\Page {
 
 
 	public function Render() {
-		return 'HOME';
+		$table = new \psm\datatables_Table(
+			array(
+				'Item',
+				'Seller',
+				'Expires',
+				'Price (Each)',
+				'Price (Total)',
+				'Market Value',
+				'Qty',
+				'Buy',
+			)
+		);
+for($i=0;$i<1000;$i++)
+		$table->addRow(
+			array(
+				'item '.$i,
+				'seller '.$i,
+				'expires',
+				'price each',
+				'price total',
+				'market value',
+				'qty',
+				'<a href="">More Options</a>',
+			)
+		);
+		return 'HOME'.$table->Render();
 	}
 
 
 	protected function Action($action) {
+	}
+
+
+}
+class home_Query extends \psm\datatables_Query {
+
+
+	public function QueryForTable() {
+		return array(
+			array('a'),
+			array('a'),
+			array('a'),
+			array('a'),
+		);
 	}
 
 
